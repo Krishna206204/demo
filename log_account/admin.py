059863259539aecd03720604545f972b482ecbff
Krishna206204 <krishnabhatta526@gmail.com>
2026-08-23 07:15:05 +0545

    a

--- log_account/admin.py
+++ log_account/admin.py
@@ -1,4 +1,47 @@
 from django.contrib import admin
 from .models import ActivityLog
-# Register your models here.
-admin.site.register(ActivityLog)
+
+
+@admin.register(ActivityLog)
+class ActivityLogAdmin(admin.ModelAdmin):
+    list_display = (
+        'log_id',
+        'user',
+        'action_type',
+        'method',
+        'path',
+        'status_code',
+        'ip_address',
+        'timestamp',
+    )
+
+    list_filter = (
+        'action_type',
+        'method',
+        'status_code',
+    )
+
+    search_fields = (
+        'user__username',
+        'action_type',
+        'path',
+        'ip_address',
+        'description',
+    )
+
+    ordering = ('-timestamp',)
+
+    readonly_fields = (
+        'log_id',
+        'user',
+        'action_type',
+        'alert_message',
+        'path',
+        'method',
+        'status_code',
+        'ip_address',
+        'description',
+        'timestamp',
+    )
+
+    list_per_page = 25
